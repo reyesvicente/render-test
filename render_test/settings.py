3136a8b8de63924f6ec63fabb87c7b63879179b3
Vicente Reyes <icenreyes@yahoo.com>
2025-04-09 19:51:04 +0800

    added allowed_host

--- render_test/settings.py
+++ render_test/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5l75)2^*5lly^i#eektcn=aadz^ai#s7#dw(zg2qak8^%4+pe%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["render-test-dtk7.onrender.com"]
 
 
 # Application definition
